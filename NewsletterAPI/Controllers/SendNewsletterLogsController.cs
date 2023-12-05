68ba1a2ab7396c6c9894ddd84589fe073957b9b7
Azadeh <azadenourani@gmail.com>
2023-12-05 16:18:36 +0330

    SendNewsToPersonnels_Every_Minute
    by cron and injection of recurring job manager

--- NewsletterAPI/Controllers/SendNewsletterLogsController.cs
+++ NewsletterAPI/Controllers/SendNewsletterLogsController.cs
@@ -1,4 +1,6 @@
 ﻿using Hangfire;
+using Hangfire.Common;
+//using Hangfire.States;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,20 +12,41 @@ namespace NewsletterAPI.Controllers
     public class SendNewsletterLogsController : Controller
     {
         private readonly ISendNewsToPersonnelListService _sendNewsToPersonnelListService;
+        private readonly IRecurringJobManager _recurringJobManager;
 
-        public SendNewsletterLogsController(ISendNewsToPersonnelListService sendNewsToPersonnelListService)
+        public SendNewsletterLogsController(ISendNewsToPersonnelListService sendNewsToPersonnelListService , IRecurringJobManager recurringJobManager)
         {
-            _sendNewsToPersonnelListService = sendNewsToPersonnelListService;
+           _sendNewsToPersonnelListService = sendNewsToPersonnelListService;
+           _recurringJobManager = recurringJobManager;
         }
 
         [HttpPost("News-log")]
         public async Task<IActionResult> Index()
         {
-            BackgroundJob.Enqueue <ISendNewsToPersonnelListService>(p => p.ExecuteAsync());
-           //await _sendNewsToPersonnelListService.ExecuteAsync();
+
+            //BackgroundJob.Enqueue <ISendNewsToPersonnelListService>(p => p.ExecuteAsync());
+            //RecurringJob.AddOrUpdate<ISendNewsToPersonnelListService>("SendNewsToPersonnelList",
+            //service => service.ExecuteAsync(), "*/5 * * * *");
+
+            //var scheduledTime = new TimeSpan(21, 40, 0);
+            //var scheduleOptions = new EnqueuedState { Queue = "default" };
+
+            //RecurringJob.AddOrUpdate<ISendNewsToPersonnelListService>("SendNewsToPersonnelList",
+            //    service => service.ExecuteAsync(), Cron.Daily(scheduledTime.Hours, scheduledTime.Minutes), scheduleOptions);
+
+            // _recurringJobManager.AddOrUpdate("", (sendNewsToPersonnelListService) => Job, Cron.Daily);
+            // _recurringJobManager.AddOrUpdate(_sendNewsToPersonnelListService, () => SendNewsletterJob(), Cron.Daily);
+
+            _recurringJobManager.AddOrUpdate("SendNewsToPersonnel", () => _sendNewsToPersonnelListService.ExecuteAsync(), Cron.Minutely);
+
+            //await _sendNewsToPersonnelListService.ExecuteAsync();
             return Ok("lastNews Sent to All prsonnels successfully");
 
         }
 
+        private object SendNewsletterJob()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
